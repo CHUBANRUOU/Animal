5f86dc27bec9e8383d676de43abace39f7185373
Duy Anh <duyanhbtm@gmail.com>
2022-04-26 07:54:36 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        public class Animal
+        {
+            public void eat()
+            {
+                Console.WriteLine("Animal is eating...");
+            }
+
+            public void makeSound()
+            {
+                Console.WriteLine("Animal is making sound...");
+            }
+        }
+
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
